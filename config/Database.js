406381b34b943ecff8110c3c37a25a79fac23f60
akbartoriq <142772211+akbartoriq@users.noreply.github.com>
2023-10-10 14:41:48 +0700

    setup for deployment to railway

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('crud_db','root','',{
-    host: 'localhost',
+const db = new Sequelize('railway','root','2qwp7cSaEcqGteOdkk8u',{
+    host: 'containers-us-west-75.railway.app',
+    port: '6372',
     dialect: 'mysql'
 });
 
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(3000, ()=> console.log('Server up and running...'));
+//app.listen(3000, ()=> console.log('Server up and running...'));
+app.listen(PORT, "0.0.0.0", function () {});
